587d23042bc1ead1cfe7d05294777135637ebb5a
tuanteng96 <tuantengyn@gmail.com>
2021-01-04 17:36:24 +0700

    first commit

---
+++ src/features/Photo/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+
+// Photo.propTypes = {
+    
+// };
+
+function Photo(props) {
+
+    const match = useRouteMatch();
+    console.log({ match });
+
+    return (
+        <Switch>
+            <Route ></Route>
+        </Switch>
+    );
+}
+
+export default Photo;
